a130eea7161b82d1e01eca105a1dc6285e4a5777
stephan281094 <stephan281094@gmail.com>
2016-09-23 12:24:14 +0200

    Improve button and fix UI glitch upon load

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ var button = document.querySelector('.o-burgr__switch')
 var overlay = document.querySelectorAll('.o-burgr__overlay:not(.o-burgr__menu)')[0]
 var content = document.querySelector('.o-burgr__content')
 var menu = document.querySelector('.' + menuClass)
-var openClass = 'o-burgr__overlay--visible'
+var openClass = 'o-burgr__overlay--open'
 
 overlay.addEventListener('click', function (evt) {
   if (evt.target.classList.contains(menuClass)) return
@@ -16,3 +16,9 @@ overlay.addEventListener('click', function (evt) {
 button.addEventListener('click', function (evt) {
   overlay.classList.toggle(openClass)
 })
+
+
+window.onload = function () {
+  // Make overlay visible via JS, to prevent seeing the menu upon loading the page
+  overlay.classList.add('o-burgr__overlay--visible')
+}
