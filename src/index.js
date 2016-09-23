8ad9e54cd4357987dab538a90cc1dd14430554ad
stephan281094 <stephan281094@gmail.com>
2016-09-23 20:20:02 +0200

    Animate overlay on dragend

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
 require('./scss/index.scss')
 
-var menuClass = 'o-burgr__menu'
-var button = document.querySelector('.o-burgr__switch')
-// var overlay = document.querySelectorAll('.o-burgr__overlay:not(.o-burgr__menu)')[0]
-var content = document.querySelector('.o-burgr__content')
-var menu = document.querySelector('.' + menuClass)
-var openClass = 'o-burgr__overlay--open'
+var menu = document.querySelector('.o-burgr__menu')
+var overlay = document.querySelector('.o-burgr__overlay')
 var container = document.querySelector('.o-burgr__container')
 
-// overlay.addEventListener('click', function (evt) {
-//   if (evt.target.classList.contains(menuClass)) return
-
-//   overlay.classList.remove(openClass)
-// })
-
-// button.addEventListener('click', function (evt) {
-//   overlay.classList.toggle(openClass)
-// })
-
-
 window.onload = function () {
-  // Make overlay visible via JS, to prevent seeing the menu upon loading the page
-  // overlay.classList.add('o-burgr__overlay--visible')
-
-
-
   // Touch support
   var containerWidth = container.getBoundingClientRect().width
   var menuWidth = menu.getBoundingClientRect().width
@@ -55,14 +35,16 @@ window.onload = function () {
     var border = -((1 - 0.5) * containerWidth)
 
     if (parseFloat(container.style.left, 10) > border) {
-      animateLeft(parseFloat(container.style.left, 10), 0)
+      animateContainerLeft(parseFloat(container.style.left, 10), 0)
+      animateLayoutOpacity(0, 1)
     } else {
-      animateLeft(parseFloat(container.style.left, 10), -menuWidth)
+      animateContainerLeft(parseFloat(container.style.left, 10), -menuWidth)
+      animateLayoutOpacity(1, 0)
     }
   }
 
   // Animations
-  function animateLeft (from, to) {
+  function animateContainerLeft (from, to) {
     var duration = 250
     var start    = new Date().getTime()
 
@@ -70,17 +52,22 @@ window.onload = function () {
       var time = new Date().getTime() - start
       var left = easeInOutQuart(time, from, to - from, duration)
       container.style.left = left + 'px'
-      if (time >= duration) clearInterval(timer);
+      if (time >= duration) clearInterval(timer)
     }, 1000 / 60)
   }
 
-  //
-  // http://easings.net/#easeInOutQuart
-  //  t: current time
-  //  b: beginning value
-  //  c: change in value
-  //  d: duration
-  //
+  function animateLayoutOpacity (from, to) {
+    var duration = 250
+    var start    = new Date().getTime()
+
+    var timer = setInterval(function () {
+      var time = new Date().getTime() - start
+      var opacity = easeInOutQuart(time, from, to - from, duration)
+      overlay.style.opacity = opacity;
+      if (time >= duration) clearInterval(timer)
+    }, 1000 / 60)
+  }
+
   function easeInOutQuart(t, b, c, d) {
     if ((t /= d / 2) < 1) return c / 2 * t * t * t * t + b;
     return -c / 2 * ((t -= 2) * t * t * t - 2) + b;
