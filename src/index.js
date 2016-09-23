e0e7cb3a436b976e8d757d0728aa3fab1d534b11
stephan281094 <stephan281094@gmail.com>
2016-09-23 18:15:14 +0200

    Add basic touch support

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ var overlay = document.querySelectorAll('.o-burgr__overlay:not(.o-burgr__menu)')
 var content = document.querySelector('.o-burgr__content')
 var menu = document.querySelector('.' + menuClass)
 var openClass = 'o-burgr__overlay--open'
+var container = document.querySelector('.o-burgr__container')
 
 overlay.addEventListener('click', function (evt) {
   if (evt.target.classList.contains(menuClass)) return
@@ -21,4 +22,38 @@ button.addEventListener('click', function (evt) {
 window.onload = function () {
   // Make overlay visible via JS, to prevent seeing the menu upon loading the page
   overlay.classList.add('o-burgr__overlay--visible')
+
+
+
+  // Touch support
+  var containerWidth = container.getBoundingClientRect().width
+  var menuWidth = menu.getBoundingClientRect().width
+  var containerX  = container.offsetLeft
+
+  container.addEventListener('touchstart', startDrag)
+  container.addEventListener('touchmove', drag)
+  container.addEventListener('touchend', endDrag)
+
+  function startDrag (evt) {
+    containerX = evt.changedTouches[0].pageX - container.offsetLeft
+  }
+
+  function drag (evt) {
+    var pageX = evt.changedTouches[0].pageX
+    var newLeft = pageX - containerX
+
+    console.log(newLeft, -containerWidth)
+
+    if (newLeft >= 0) {
+      newLeft = 0
+    } else if (newLeft <= -menuWidth) {
+      newLeft = -menuWidth
+    }
+
+    container.style.left = newLeft + 'px'
+  }
+
+  function endDrag (evt) {
+
+  }
 }
