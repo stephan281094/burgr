c34004033c3f11068a9d53b0a708d5c603252acb
stephan281094 <stephan281094@gmail.com>
2016-09-24 19:00:30 +0200

    Fix the button

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ require('./scss/index.scss')
 var menu = document.querySelector('.o-burgr__menu')
 var overlay = document.querySelector('.o-burgr__overlay')
 var container = document.querySelector('.o-burgr__container')
+var button = document.querySelector('.o-burgr__switch')
+var isOpen = false
 
 window.onload = function () {
   // Touch support
@@ -13,6 +15,21 @@ window.onload = function () {
   container.addEventListener('touchstart', startDrag)
   container.addEventListener('touchmove', drag)
   container.addEventListener('touchend', endDrag)
+  container.addEventListener('click', click)
+  button.addEventListener('click', open)
+  menu.addEventListener('click', function (e) { e.stopPropagation() })
+
+  function open () {
+    animateContainerLeft(-menuWidth, 0, 350)
+    animateLayoutOpacity(0, 1)
+    isOpen = true
+  }
+
+  function close () {
+    animateContainerLeft(0, -menuWidth, 350)
+    animateLayoutOpacity(1, 0)
+    isOpen = false
+  }
 
   function startDrag (evt) {
     containerX = evt.changedTouches[0].pageX - container.offsetLeft
@@ -35,18 +52,30 @@ window.onload = function () {
   function endDrag (evt) {
     var border = -((1 - 0.5) * containerWidth)
 
+    // If user dragged beyond 'border'
     if (parseFloat(container.style.left, 10) > border) {
       animateContainerLeft(parseFloat(container.style.left, 10), 0)
       animateLayoutOpacity(parseFloat(overlay.style.opacity, 10), 1)
+      isOpen = true
     } else {
       animateContainerLeft(parseFloat(container.style.left, 10), -menuWidth)
       animateLayoutOpacity(parseFloat(overlay.style.opacity, 10), 0)
+      isOpen = false
+    }
+  }
+
+  function click (evt) {
+    if (isOpen) {
+      animateContainerLeft(parseFloat(container.style.left, 10), -menuWidth, 350)
+      animateLayoutOpacity(parseFloat(overlay.style.opacity, 10), 0, 350)
+
+      isOpen = false
     }
   }
 
   // Animations
-  function animateContainerLeft (from, to) {
-    var duration = 250
+  function animateContainerLeft (from, to, duration) {
+    var duration = duration || 250
     var start    = new Date().getTime()
 
     var timer = setInterval(function () {
@@ -57,8 +86,8 @@ window.onload = function () {
     }, 1000 / 60)
   }
 
-  function animateLayoutOpacity (from, to) {
-    var duration = 250
+  function animateLayoutOpacity (from, to, duration) {
+    var duration = duration || 250
     var start    = new Date().getTime()
 
     var timer = setInterval(function () {
